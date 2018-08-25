9afead1aebdd5a538f261edc912a9e445d834f5a
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-08-24 20:10:37 -0400

    style(Pipe):

--- dist/pipe.js
+++ dist/pipe.js
@@ -1,28 +1,28 @@
-'use strict';
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
-});
+})
 
-var _inArray = require('./inArray');
+var _inArray = require('./inArray')
 
-var _inArray2 = _interopRequireDefault(_inArray);
+var _inArray2 = _interopRequireDefault(_inArray)
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
 
-var compose = function compose(acc, act, index, array) {
-  return acc instanceof Array ? (0, _inArray2.default)(act)(acc) : act(acc);
-};
+var compose = function compose (acc, act, index, array) {
+  return acc instanceof Array ? (0, _inArray2.default)(act)(acc) : act(acc)
+}
 
-var pipe = function pipe() {
+var pipe = function pipe () {
   for (var _len = arguments.length, functions = Array(_len), _key = 0; _key < _len; _key++) {
-    functions[_key] = arguments[_key];
+    functions[_key] = arguments[_key]
   }
 
-  var variable = functions.shift();
+  var variable = functions.shift()
   if (functions.length < 1) {
-    throw new TypeError('No function to be appllied');
+    throw new TypeError('No function to be appllied')
   }
-  return functions.reduce(compose, variable);
-};
-exports.default = pipe;
+  return functions.reduce(compose, variable)
+}
+exports.default = pipe
