ef62a6e36bf4282d03cb78b79f625a8ee4b6e111
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-08-24 21:01:50 -0400

    style(Lib): corrigido erros de lint

--- dist/index.js
+++ dist/index.js
@@ -1,50 +1,52 @@
-'use strict';
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
-});
+})
 
-var _all = require('./all');
+var _all = require('./all')
 
-var _all2 = _interopRequireDefault(_all);
+var _all2 = _interopRequireDefault(_all)
 
-var _div = require('./div');
+var _div = require('./div')
 
-var _div2 = _interopRequireDefault(_div);
+var _div2 = _interopRequireDefault(_div)
 
-var _groupBy = require('./groupBy');
+var _groupBy = require('./groupBy')
 
-var _groupBy2 = _interopRequireDefault(_groupBy);
+var _groupBy2 = _interopRequireDefault(_groupBy)
 
-var _has = require('./has');
+var _has = require('./has')
 
-var _has2 = _interopRequireDefault(_has);
+var _has2 = _interopRequireDefault(_has)
 
-var _inArray = require('./inArray');
+var _inArray = require('./inArray')
 
-var _inArray2 = _interopRequireDefault(_inArray);
+var _inArray2 = _interopRequireDefault(_inArray)
 
-var _log = require('./log');
+var _log = require('./log')
 
-var _log2 = _interopRequireDefault(_log);
+var _log2 = _interopRequireDefault(_log)
 
-var _mul = require('./mul');
+var _mul = require('./mul')
 
-var _mul2 = _interopRequireDefault(_mul);
+var _mul2 = _interopRequireDefault(_mul)
 
-var _pipe = require('./pipe');
+var _pipe = require('./pipe')
 
-var _pipe2 = _interopRequireDefault(_pipe);
+var _pipe2 = _interopRequireDefault(_pipe)
 
-var _sub = require('./sub');
+var _sub = require('./sub')
 
-var _sub2 = _interopRequireDefault(_sub);
+var _sub2 = _interopRequireDefault(_sub)
 
-var _sum = require('./sum');
+var _sum = require('./sum')
 
-var _sum2 = _interopRequireDefault(_sum);
+var _sum2 = _interopRequireDefault(_sum)
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault (obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
 
 exports.default = {
   all: _all2.default,
@@ -57,4 +59,4 @@ exports.default = {
   pipe: _pipe2.default,
   sub: _sub2.default,
   sum: _sum2.default
-};
+}
--- test/all.test.js
+++ test/all.test.js
@@ -1,4 +1,4 @@
-const {all} = require('../index')
+const { all } = require('../index')
 
 test('all method should not be undefined', () => {
   expect(all).toBeTruthy()
@@ -12,30 +12,29 @@ test('all method has params: Function and Array', () => {
   const fooFunction = () => {}
   const fooArray = []
 
-    expect(() => all(null)(null)).toThrowError(TypeError)
-    expect(() => all(fooFunction)(null)).toThrowError(TypeError)
-    expect(() => all(null)(fooArray)).toThrowError(TypeError)
-    expect(() => all(fooFunction)(fooArray)).not.toThrowError(TypeError)
+  expect(() => all(null)(null)).toThrowError(TypeError)
+  expect(() => all(fooFunction)(null)).toThrowError(TypeError)
+  expect(() => all(null)(fooArray)).toThrowError(TypeError)
+  expect(() => all(fooFunction)(fooArray)).not.toThrowError(TypeError)
 })
 
 test('check if all items are even', () => {
   const isEven = i => i % 2 !== 0
-    const isOdd = i => i % 2 == 0
+  const isOdd = i => i % 2 == 0
 
-        expect(all(isEven)([0, 1, 2])).toBe(false)
-            expect(all(isEven)([1])).toBe(true)
-                expect(all(isOdd)([1])).toBe(false)
-      expect(all(isEven)([1, 3, 23])).toBe(true)
-        expect(all(isEven)([])).toBe(false)
+  expect(all(isEven)([0, 1, 2])).toBe(false)
+  expect(all(isEven)([1])).toBe(true)
+  expect(all(isOdd)([1])).toBe(false)
+  expect(all(isEven)([1, 3, 23])).toBe(true)
+  expect(all(isEven)([])).toBe(false)
 
-          expect(all(isOdd)([])).toBe(false)
-            expect(all(isOdd)([2])).toBe(true)
-              expect(all(isEven)([2])).toBe(false)
-            expect(all(isOdd)([2, 4, 8])).toBe(true)
-                expect(all(isOdd)([0, 1, 2])).toBe(false)
+  expect(all(isOdd)([])).toBe(false)
+  expect(all(isOdd)([2])).toBe(true)
+  expect(all(isEven)([2])).toBe(false)
+  expect(all(isOdd)([2, 4, 8])).toBe(true)
+  expect(all(isOdd)([0, 1, 2])).toBe(false)
 
-
-                  expect(all(isEven)([null])).toBe(false)
-                expect(all(isOdd)([null, 4, undefined])).toBe(false)
-                    expect(all(isOdd)([undefined])).toBe(false)
+  expect(all(isEven)([null])).toBe(false)
+  expect(all(isOdd)([null, 4, undefined])).toBe(false)
+  expect(all(isOdd)([undefined])).toBe(false)
 })
