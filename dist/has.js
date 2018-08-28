cede1bd02efcdd8e4144da69b0a4639bf7f138d4
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-08-27 21:06:04 -0400

    style(): formatado usando prettier

--- dist/has.js
+++ dist/has.js
@@ -1,21 +1,22 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
 var hasKeyObject = function hasKeyObject(attr) {
-  return function (obj) {
-    if (obj) if (obj[attr]) return true;else if (obj[attr] === 0) return true;
+  return function(obj) {
+    if (obj)
+      if (obj[attr]) return true;
+      else if (obj[attr] === 0) return true;
 
     return false;
   };
 };
 
 var has = function has(attr) {
-  return function (obj) {
-
+  return function(obj) {
     if (attr instanceof Array) {
-      return attr.reduce(function (acc, act) {
+      return attr.reduce(function(acc, act) {
         return acc && hasKeyObject(act)(obj);
       }, attr.length > 0);
     }
