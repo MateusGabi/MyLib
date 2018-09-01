c9dddff94bccf136b35fee14d48cfd9b0974e810
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-09-01 09:44:44 -0400

    perf(all):

--- dist/has.js
+++ dist/has.js
@@ -1,28 +1,27 @@
-'use strict'
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
-})
-var hasKeyObject = function hasKeyObject (attr) {
+});
+var hasKeyObject = function hasKeyObject(attr) {
   return function (obj) {
     if (obj) {
-      if (obj[attr]) return true
-      else if (obj[attr] === 0) return true
+      if (obj[attr]) return true;else if (obj[attr] === 0) return true;
     }
 
-    return false
-  }
-}
+    return false;
+  };
+};
 
-var has = function has (attr) {
+var has = function has(attr) {
   return function (obj) {
     if (attr instanceof Array) {
       return attr.reduce(function (acc, act) {
-        return acc && hasKeyObject(act)(obj)
-      }, attr.length > 0)
+        return acc && hasKeyObject(act)(obj);
+      }, attr.length > 0);
     }
 
-    return hasKeyObject(attr)(obj)
-  }
-}
-exports.default = has
+    return hasKeyObject(attr)(obj);
+  };
+};
+exports.default = has;
