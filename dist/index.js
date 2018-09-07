fe583c0a36f5fa519427c62779b63e14ef631137
Mateus Gabi <mateusgabimoreira@hotmail.com>
2018-09-06 21:26:45 -0400

    style(): formatado usando eslint

--- dist/index.js
+++ dist/index.js
@@ -1,55 +1,55 @@
-'use strict';
+'use strict'
 
 Object.defineProperty(exports, '__esModule', {
-  value: true,
-});
+  value: true
+})
 
-var _all = require('./all');
+var _all = require('./all')
 
-var _all2 = _interopRequireDefault(_all);
+var _all2 = _interopRequireDefault(_all)
 
-var _div = require('./div');
+var _div = require('./div')
 
-var _div2 = _interopRequireDefault(_div);
+var _div2 = _interopRequireDefault(_div)
 
-var _groupBy = require('./groupBy');
+var _groupBy = require('./groupBy')
 
-var _groupBy2 = _interopRequireDefault(_groupBy);
+var _groupBy2 = _interopRequireDefault(_groupBy)
 
-var _has = require('./has');
+var _has = require('./has')
 
-var _has2 = _interopRequireDefault(_has);
+var _has2 = _interopRequireDefault(_has)
 
-var _inArray = require('./inArray');
+var _inArray = require('./inArray')
 
-var _inArray2 = _interopRequireDefault(_inArray);
+var _inArray2 = _interopRequireDefault(_inArray)
 
-var _log = require('./log');
+var _log = require('./log')
 
-var _log2 = _interopRequireDefault(_log);
+var _log2 = _interopRequireDefault(_log)
 
-var _mul = require('./mul');
+var _mul = require('./mul')
 
-var _mul2 = _interopRequireDefault(_mul);
+var _mul2 = _interopRequireDefault(_mul)
 
-var _pairs = require('./pairs');
+var _pairs = require('./pairs')
 
-var _pairs2 = _interopRequireDefault(_pairs);
+var _pairs2 = _interopRequireDefault(_pairs)
 
-var _pipe = require('./pipe');
+var _pipe = require('./pipe')
 
-var _pipe2 = _interopRequireDefault(_pipe);
+var _pipe2 = _interopRequireDefault(_pipe)
 
-var _sub = require('./sub');
+var _sub = require('./sub')
 
-var _sub2 = _interopRequireDefault(_sub);
+var _sub2 = _interopRequireDefault(_sub)
 
-var _sum = require('./sum');
+var _sum = require('./sum')
 
-var _sum2 = _interopRequireDefault(_sum);
+var _sum2 = _interopRequireDefault(_sum)
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
+function _interopRequireDefault (obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
 }
 
 exports.default = {
@@ -63,5 +63,5 @@ exports.default = {
   pairs: _pairs2.default,
   pipe: _pipe2.default,
   sub: _sub2.default,
-  sum: _sum2.default,
-};
+  sum: _sum2.default
+}
--- dist/pairs.js
+++ dist/pairs.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
-   value: true
-});
-var reducer = function reducer(acc, cur, index, arr) {
-   for (var i = index + 1; i < arr.length; ++i) {
-      acc.push([cur, arr[i]]);
-   }
-   return acc;
-};
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+var reducer = function reducer (acc, cur, index, arr) {
+  for (var i = index + 1; i < arr.length; ++i) {
+    acc.push([cur, arr[i]])
+  }
+  return acc
+}
 
-var pairs = function pairs(arr) {
-   return arr.reduce(reducer, []);
-};
+var pairs = function pairs (arr) {
+  return arr.reduce(reducer, [])
+}
 
-exports.default = pairs;
+exports.default = pairs
